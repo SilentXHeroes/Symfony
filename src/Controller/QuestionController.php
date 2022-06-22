6fbc22f580a06b25c6a7e77c7c0fd34020e3028d
SilentXHeroes <giorgio86@outlook.com>
2022-06-22 19:37:46 +0200

    TEST

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,23 +2,50 @@
 
 namespace App\Controller;
 
+use Twig\Environment;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Cache\CacheInterface;
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
 	/**
-	 * @Route("/")
+	 * @Route("/", name="app_homepage")
 	 */
-	public function homepage()
+	public function homepage(Environment $twigEnv)
 	{
-		return new Response('Some text !');
+		/*
+		// fun example of using the Twig service directly
+		$html = $twigEnv->render("question/homepage.html.twig");
+		return new Response($html);
+		*/
+		return $this->render("question/homepage.html.twig");
 	}
 
 	/**
-	 * @Route("/questions/{slug}")
+	 * @Route("/questions/{slug}", name="app_question_show")
 	 */
-	public function show($slug) {
-		return new Response(sprintf('Future page "%s" !', str_replace('-', ' ', $slug)));
+	public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache) {
+		$answers = [
+			"Réponse numéro 1",
+			"Réponse numéro 2 un peu `plus` vénère",
+			"Réponse numéro 3 EXTREMEMENT VENERE WOUAH"
+		];
+
+		$questionText = "I've been turned into a cat, any *thoughts* on how to turn back? While I'm **adorable**, I don't really care for cat food.";
+
+		$parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($questionText, $markdownParser) {
+			return $markdownParser->transformMarkdown($questionText);
+		});
+
+		dd($markdownParser);
+
+		return $this->render('question/show.html.twig', [
+			'question' => ucwords(str_replace('-', ' ', $slug)),
+			'answers' => $answers,
+			'questionText' => $parsedQuestionText
+		]);
 	}
 }
